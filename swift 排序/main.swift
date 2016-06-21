8df4cb6711cf6a4bc89deefe0a698323f7513d18
wwwwenxin <578355367@qq.com>
2016-06-21 13:11:55 +0800

    swift 排序
    先用var定义3个变量并赋值，再定义一个数组并赋值。
    然后用for循环，并在其中继续嵌套一个for循环，使i循环20次，再将数据遍历一遍，没有遗憾。
    在循环中如果i的值小于j的值，则把i的值赋给temp，把的值赋给i，把temp的值赋给j,最后利用value使数据哟行输出一个

--- swift 排序/main.swift
+++ swift 排序/main.swift
@@ -7,6 +7,20 @@
 //
 
 import Foundation
-
 print("Hello, World!")
 
+var i=0 ,j=0 ,temp=0
+var List = [54,39,32,65,34,23,43,65,12,64,87,97,23,13,43,52,76,98,15,54]
+for j in 0...19{
+    for i in 0...19{
+        if(List[i]>List[j])
+        {
+            temp=List[i]
+            List[i]=List[j]
+            List[j]=temp
+        }
+    }
+}
+for value in List{
+    print(value)
+}
